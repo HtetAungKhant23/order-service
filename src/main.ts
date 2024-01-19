c13289d2933e91e0f69cf0d591b7e4fa9e84a131
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-19 17:31:25 +0630

    order service and prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
+  app.startAllMicroservices();
+  await app.listen(3005).then(() => {
+    Logger.log('🚀 Order Service Successfully started at 3005');
+  });
 }
 bootstrap();
